e2edbc7aa9cc2133c9a5267a03e2babfdf844454
infosanuth <infosanuth@gmail.com>
2026-05-02 13:24:10 +0530

    feat: add movies dataset for featured section

---
+++ client/src/assets/assets.js
@@ -0,0 +1,78 @@
+const movies = [
+
+    {
+        id: 1,
+        title: "The Dark Knight",
+        poster: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+        runtime: "2h 32m"
+    },
+    {
+        id: 2,
+        title: "Avengers: Endgame",
+        poster: "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
+        runtime: "3h 1m"
+    },
+    {
+        id: 3,
+        title: "Joker",
+        poster: "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
+        runtime: "2h 2m"
+    },
+    {
+        id: 4,
+        title: "Titanic",
+        poster: "https://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
+        runtime: "3h 14m"
+    },
+    {
+        id: 5,
+        title: "The Matrix",
+        poster: "https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg",
+        runtime: "2h 16m"
+    },
+    {
+        id: 6,
+        title: "Spider-Man: No Way Home",
+        poster: "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+        runtime: "2h 28m"
+    },
+    {
+        id: 7,
+        title: "Guardians of the Galaxy",
+        poster: "https://image.tmdb.org/t/p/w500/r7vmZjiyZw9rpJMQJdXpjgiCOk9.jpg",
+        runtime: "2h 1m"
+    },
+    {
+        id: 8,
+        title: "Black Panther",
+        poster: "https://image.tmdb.org/t/p/w500/uxzzxijgPIY7slzFvMotPv8wjKA.jpg",
+        runtime: "2h 14m"
+    },
+    {
+        id: 9,
+        title: "Thor: Ragnarok",
+        poster: "https://image.tmdb.org/t/p/w500/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg",
+        runtime: "2h 10m"
+    },
+    {
+        id: 10,
+        title: "Avatar",
+        poster: "https://image.tmdb.org/t/p/w500/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg",
+        runtime: "2h 42m"
+    },
+    {
+        id: 11,
+        title: "John Wick",
+        poster: "https://image.tmdb.org/t/p/w500/fZPSd91yGE9fCcCe6OoQr6E3Bev.jpg",
+        runtime: "1h 41m"
+    },
+    {
+        id: 12,
+        title: "Forrest Gump",
+        poster: "https://image.tmdb.org/t/p/w500/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg",
+        runtime: "2h 22m"
+    },
+
+];
+
+export default movies;
